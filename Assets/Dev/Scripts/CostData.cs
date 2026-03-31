61de63169ae1f5d03992e968f55f1ee50298f048
PhonedLeek25 <omardiaa2004@gmail.com>
2026-03-31 00:22:04 -0400

    forgor to uplaod this one too

--- Assets/Dev/Scripts/CostData.cs
+++ Assets/Dev/Scripts/CostData.cs
@@ -2,17 +2,35 @@ using System.Collections.Generic;
 using UnityEngine;
 public enum TextureTypes
 {
-    None, Wall, Ceiling, Floor, Furniture
+    None, Wall, Ceiling, Floor, FFE
 }
-public class Texture
+public enum CostUnits
 {
-    public string name;
-    public int cost;
-    public TextureTypes TextureType;
+    None, SF, LF, EA
+}
 
-    public Texture(string name, int cost)
+public class ObjectInfo
+{
+    public string Name;
+    //public string textureName;
+    public Texture texture;
+    public int TextureCostPerUnit;
+    public int CurrentCost;
+    public ObjectInfo(string Name, Texture texture, int TextureCostPerUnit, int CurrentCost)
     {
-        this.name = name;
-        this.cost = cost;
+        this.Name = Name;
+        this.texture = texture;
+        this.TextureCostPerUnit = TextureCostPerUnit;
+        this.CurrentCost = CurrentCost;
+    }
+}
+
+public class CostData : MonoBehaviour
+{
+    [SerializeField]
+    public List<ObjectInfo> ObjectsInScene = new List<ObjectInfo>();
+    public void AddItem(string Name, Texture texture = null, int TextureCostPerUnit = 0, int CurrentCost = 0)
+    {
+        ObjectsInScene.Add(new ObjectInfo(Name, texture, TextureCostPerUnit, CurrentCost));
     }
 }
