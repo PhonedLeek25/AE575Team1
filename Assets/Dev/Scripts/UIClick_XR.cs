420f4d6b072326c2687101009aecd010baa4ef15
PhonedLeek25 <omardiaa2004@gmail.com>
2026-04-28 17:37:51 -0400

    updated Script

--- Assets/Dev/Scripts/UIClick_XR.cs
+++ Assets/Dev/Scripts/UIClick_XR.cs
@@ -1,43 +1,20 @@
 ﻿using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.SceneManagement;
 
-//[RequireComponent(typeof(UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable))]
 [RequireComponent(typeof(Collider))]
+
 public class UIClick_XR : MonoBehaviour
 {
     //private XRBaseInteractable interactable;
     public GameObject objToHide;
     public GameObject objToShow;
-
-    //void Awake()
-    //{
-    //    interactable = GetComponent<XRBaseInteractable>();
-    //
-    //    if (!TryGetComponent(out Collider col))
-    //    {
-    //        Debug.LogError("No Collider found on " + gameObject.name);
-    //    }
-    //
-    //    interactable.selectEntered.AddListener(OnClicked);
-    //}
-    //void OnDestroy()
-    //{
-    //    if (interactable != null)
-    //    {
-    //        interactable.selectEntered.RemoveListener(OnClicked);
-    //    }
-    //}
-    //void OnClicked(SelectEnterEventArgs args)
-    //{
-    //    Debug.Log("Object clicked: " + gameObject.name);
-    //
-    //    CustomFunction();
-    //}
+    public bool LoadScene;
+    public string sceneToLoad;
     public void CustomFunction()
     {
         Debug.Log("executing custom function!");
         objToShow.SetActive(true);
         objToHide.SetActive(false);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
